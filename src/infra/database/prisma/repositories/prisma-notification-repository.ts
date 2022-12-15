00b11415e3dcb09f9fcd15e6284d858e8fea5aa6
Dsblima <dsbl@ecomp.poli.br>
2022-12-15 09:26:52 -0300

    Create mappers and viewmodel

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -1,22 +1,18 @@
 import { Notification } from "src/application/entities/notification";
-import { NotificationsRepository } from "../../../../application/repositories/notifications-repository";
+import { NotificationsRepository } from "@application/repositories/notifications-repository";
 import { PrismaService } from "../prisma.service";
 import { Injectable } from "@nestjs/common";
+import { PrismaNotificationMapper } from "../mappers/prisma-notification-mapper";
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      }
+      data: raw
     });  
   }
 
