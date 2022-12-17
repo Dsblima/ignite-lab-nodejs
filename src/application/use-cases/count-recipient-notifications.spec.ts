65391b919006fa698b49a4fa2a12a162dbe3ca37
Dsblima <dsbl@ecomp.poli.br>
2022-12-17 15:32:22 -0300

    Remove unsed imports

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,10 +1,6 @@
-import { Content } from "@application/entities/content";
-import { Notification } from "@application/entities/notification";
 import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
-import { CancelNotification } from "./cancel-notification";
 import { CountRecipientNotification } from "./count-recipient-notifications";
-import { NotificationNotFound } from "./errors/notification-not-found";
 
 describe('Count recipient Notification', () => {
   it('should be able to count recipient notifications', async () => {
--- src/application/use-cases/get-recipient-notifications.spec.ts
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -1,10 +1,5 @@
-import { Content } from "@application/entities/content";
-import { Notification } from "@application/entities/notification";
 import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
-import { CancelNotification } from "./cancel-notification";
-import { CountRecipientNotification } from "./count-recipient-notifications";
-import { NotificationNotFound } from "./errors/notification-not-found";
 import { GetRecipientNotification } from "./get-recipient-notifications";
 
 describe('Count recipient Notification', () => {
